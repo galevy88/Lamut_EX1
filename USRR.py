bed3e169787bdb0afd7616a9523f61181b61f32c
Gal Levy <gallevy88@gmail.com>
2022-11-23 15:29:10 +0200

    Finishing USRR

---
+++ USRR.py
@@ -0,0 +1,40 @@
+
+class USRR:
+    def __init__(self, DNA):
+        self.DNA = DNA
+        self.frontier = []
+        self.SRR = {}
+
+    
+    def find_srr_one_time(self,dna, sr, s):
+        work_dna = dna[s:]
+        self.frontier = self.split(work_dna, sr)
+        strike = 1
+        while self.frontier:
+            current = self.frontier.pop(0)
+            if self.frontier != []:
+                if (current == self.frontier[0]):
+                    strike += 1
+                else: strike = 1
+            if strike >= 3:
+                self.SRR[current] = strike
+        
+
+    def find_all_srr(self):
+        srr_size = [1, 2, 3, 4, 5, 6]
+        for sr in srr_size:
+            for s in range(0, sr):
+                cp_dna = self.DNA
+                self.find_srr_one_time(cp_dna, sr , s)
+        return self.create_ls(self.SRR)
+    
+    def split(self, worked_dna, sr):
+        ls = [worked_dna[i:i+sr] for i in range(0, len(worked_dna), sr)]
+        return ls
+
+    def create_ls(self, dict):
+        ls = []
+        for key in dict:
+            ls.append((key, dict[key]))
+        return ls
+
